0d9a2d438135973372e46762c85ebc3301f04c5b
Krxyz09 <jindalkrish25@gmail.com>
2026-01-03 02:56:50 +0530

    update done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Route, Routes, useLocation} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Cars'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
+  const isownerpath = useLocation().pathname.startsWith('/owner')
   return (
-    <div>
-      
-    </div>
+    <>
+      {!isownerpath && <Navbar setShowLogin={setShowLogin}/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+      </Routes>
+    </>
   )
 }
 
